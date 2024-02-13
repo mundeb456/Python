160376f897acf0bef56b13d1d8aada5fb25a8647
mundeb456 <94440516+mundeb456@users.noreply.github.com>
2024-02-13 18:56:57 +0530

    Add one comment in app.py file. (#1)
    * Add one comment in app.py file.
    
    * add workflow
    
    * update runner
    
    * adjust code
    
    * add Werkzeug package

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return jsonify({'message': 'Hello, World!'})
+    @app.route('/')
+    def hello():
+        return jsonify({'message': 'Hello, World!'})
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
